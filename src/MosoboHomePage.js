53fdc95252d5c57ca56392ababc5d9ce421c0be0
reeshy <64738767+reeshy@users.noreply.github.com>
2023-02-20 17:30:48 +0100

    first commit

---
+++ src/MosoboHomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function MosoboHomePage() {
+  return (
+    <div>
+      <h1>Welcome to Mosobo!</h1>
+      <p>Here you can browse our collection of movies, songs, and books.</p>
+      <p>Use the navigation menu to explore our collection.</p>
+      <p>Thank you for visiting Mosobo!</p>
+    </div>
+  );
+}
---
+++ src/MoviesPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MoviesPage() {
+  return (
+    <div>
+      <h1>Movies</h1>
+      <p>Here are some of our featured movies:</p>
+      <ul>
+        <li>The Godfather</li>
+        <li>Star Wars</li>
+        <li>The Shawshank Redemption</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MoviesPage;
