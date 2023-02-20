19184398d644a29a9d46c4a3fc9a5a49a1b73bf1
reeshy <64738767+reeshy@users.noreply.github.com>
2023-02-20 18:00:31 +0100

    fix

--- src/MosoboHomePage.js
+++ src/MosoboHomePage.js
@@ -1,15 +1,12 @@
-import { Link } from "react-router-dom";
+import React from "react";
 
-function MosoboHomePage() {
+export default function MosoboHomePage() {
   return (
     <div>
       <h1>Welcome to Mosobo!</h1>
       <p>Here you can browse our collection of movies, songs, and books.</p>
       <p>Use the navigation menu to explore our collection.</p>
       <p>Thank you for visiting Mosobo!</p>
-      <Link to="/movies">Browse Movies</Link>
     </div>
   );
 }
-
-export default MosoboHomePage;
